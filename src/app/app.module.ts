94cd0420901d240add024602455bd466b63aa730
Ab Reitsma <abreits@gmail.com>
2022-06-01 23:45:48 +0200

    added angular material and fontsl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TextAreaComponent } from './components/text-area/text-area.component';
 import { BackgroundComponent } from './components/background/background.component';
+import { MaterialModule } from './shared/modules/material/material.module';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { BackgroundComponent } from './components/background/background.componen
     BackgroundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/text-area/text-area.component.ts
+++ src/app/components/text-area/text-area.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./text-area.component.scss']
 })
 export class TextAreaComponent implements OnInit {
+  private minimized = true;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  minimize(): void {
+    if (this.minimized) {
+
+    }
+  }
+
+  toggleFullScreen() {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      }
+    }
   }
 
 }
