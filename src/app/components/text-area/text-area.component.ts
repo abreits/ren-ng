bc9fe54648fca9d631d620c72dd3b38e65dd587c
Ab Reitsma <ab.reitsma@politie.nl>
2022-06-03 10:11:20 +0200

    added preliminary hide textarea

--- src/app/components/text-area/text-area.component.ts
+++ src/app/components/text-area/text-area.component.ts
@@ -1,32 +1,50 @@
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-text-area',
+  animations: [
+    trigger('displayHide', [
+      state('display', style({
+        top: '75%',
+        bottom: '0%',
+        opacity: '100%'
+      })),
+      state('hide', style({
+        top: '100%',
+        bottom: '-25%',
+        opacity: '0%'
+      })),
+      transition('display => hide', [
+        animate('0.2s')
+      ]),
+      transition('hide => display', [
+        animate('0.2s')
+      ])
+    ])
+  ],
   templateUrl: './text-area.component.html',
   styleUrls: ['./text-area.component.scss']
 })
 export class TextAreaComponent implements OnInit {
-  private minimized = true;
+  hide = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  minimize(): void {
-    if (this.minimized) {
-
-    }
+  toggle(): void {
+    this.hide = !this.hide;
   }
 
   toggleFullScreen() {
     if (!document.fullscreenElement) {
-        document.documentElement.requestFullscreen();
+      document.documentElement.requestFullscreen();
     } else {
       if (document.exitFullscreen) {
         document.exitFullscreen();
       }
     }
   }
-
 }
